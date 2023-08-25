3106cbc5fe24d969cb59fd36b525bc2d0ecc3a34
Dharmaraj Jena <113529989+codespike9@users.noreply.github.com>
2023-08-26 00:28:03 +0530

    vercel setup

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-68u4sa7qa@k3h5k31tz43db@g!4y=9!%qr07nr^&u=so67w1)^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
